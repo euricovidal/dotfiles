23cd5c8d36597be86b4c01ebf80588a3c24d7e57
Eurico Vidal <euricovidal@gmail.com>
2016-10-10 21:38:34 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ HERE=`pwd`
 BKP_PATH=$HERE/tmp/backup
 GREEN="\e[1;32m"
 NO_COLOR="\e[0m"
+OSX=$(uname -a | grep -o "^Darwin")
 
 function log() {
   local type=$1
@@ -44,10 +45,18 @@ read RUBY
 log "Use zsh [Y/n]:"
 read ZSH
 
+log "Use vim [Y/n]:"
+read VIM
+
 log "Use tmux [Y/n]: "
 read TMUX
 
-if [ "$GIT_EMAIL" == "" ] && [ "$RUBY" == "n" ] && [ "$ZSH" == "n" ] && [ "$TMUX" == "n" ]; then
+if [ "$TMUX" != "n" ]; then
+	log "Simple tmux config (if no, will be set the pretty config) [Y/n]: "
+	read TMUX_SIMPLE
+fi
+
+if [ "$GIT_EMAIL" == "" ] && [ "$RUBY" == "n" ] && [ "$ZSH" == "n" ] && [ "$TMUX" == "n" ] && [ "$VIM" == "n" ]; then
 	log "error" "Do nothing, bye..."
 	exit
 fi
@@ -59,7 +68,14 @@ echo "\n"
 [ "$GIT_EMAIL" != "" ] && log "info" "\t- Set GitHub (email: $GIT_EMAIL)\n"
 [ "$RUBY" != "n" ] && log "info" "\t- Set Ruby configs\n"
 [ "$ZSH" != "n" ] && log "info" "\t- Install ZSH and set configs\n"
-[ "$TMUX" != "n" ] && log "info" "\t- Install TMUX and set configs\n"
+[ "$VIM" != "n" ] && log "info" "\t- Install Vim and set configs\n"
+if [ "$TMUX" != "n" ]; then
+	if [ "$TMUX_SIMPLE" != "n" ]; then
+		log "info" "\t- Install TMUX and set simple configs\n"
+	else
+		log "info" "\t- Install TMUX and set pretty configs\n"
+	fi
+fi
 
 echo "\n"
 
@@ -77,72 +93,96 @@ if [ "$GIT_EMAIL" != "" ]; then
 	log "info" "Moving git configs to tmp/backup\n"
 	mv -f $HOME/.gitconfig $HOME/.gitignore $BKP_PATH
 	log "info" "Seting new configs to git\n"
-	ln -sf $HERE/gitconfig $HOME/.gitconfig
-	ln -sf $HERE/gitignore $HOME/.gitignore
+	ln -s -f $HERE/gitconfig $HOME/.gitconfig
+	ln -s -f $HERE/gitignore $HOME/.gitignore
 fi
 
 if [ "$RUBY" != "n" ]; then
 	log "info" "Moving ruby configs to tmp/backup\n"
 	mv -f $HOME/.irbrc $HOME/.gemrc $HOME/.rspec  $BKP_PATH
 	log "info" "Seting new configs to ruby\n"
-	ln -sf $HERE/inputrc $HOME/.inputrc
-	ln -sf $HERE/gemrc $HOME/.gemrc
-	ln -sf $HERE/rspec $HOME/.rspec
-	ln -sf $HERE/irbrc $HOME/.irbrc
+	ln -s -f $HERE/inputrc $HOME/.inputrc
+	ln -s -f $HERE/gemrc $HOME/.gemrc
+	ln -s -f $HERE/rspec $HOME/.rspec
+	ln -s -f $HERE/irbrc $HOME/.irbrc
 fi
 
 if [ "$ZSH" != "n" ]; then
 	log "info" "Moving zsh configs to tmp/backup\n"
 	mv -f $HOME/.zshrc $BKP_PATH
 	if [ "$ZSH_VERSION" == "" ]; then
-		log "info" "Install zsh\n"
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+		log "info" "After all steps will be installed zsh\n"
 	fi
-	mkdir -p ${ZSH_CUSTOM}/plugins
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-	git clone https://github.com/caarlos0/zsh-open-pr.git ${ZSH_CUSTOM}/plugins/zsh-open-pr
-	git clone git://github.com/zsh-users/zaw.git ${ZSH_CUSTOM}/plugins/zaw
+	mkdir -p ~/.oh-my-zsh/custom/plugins
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git clone https://github.com/caarlos0/zsh-open-pr.git ~/.oh-my-zsh/custom/plugins/zsh-open-pr
+	git clone git://github.com/zsh-users/zaw.git ~/.oh-my-zsh/custom/plugins/zaw
 	log "info" "Seting new configs to zsh\n"
-	ln -sf $HERE/zshrc $HOME/.zshrc
+	ln -s -f $HERE/zshrc $HOME/.zshrc
 else
 	log "info" "Moving bash configs to tmp/backup\n"
 	mv -f $HOME/.bash $HOME/.bash_history $BKP_PATH
 	log "info" "Seting new configs to bash\n"
-	ln -sf $HERE/bash_profile $HOME/.bash_profile
-	ln -sf $HERE/bash_profile $HOME/.bashrc
-	ln -sf $HERE/bash $HOME/.bash
-	ln -sf $HERE/bash_completion /etc/bash_completion
+	ln -s -f $HERE/bash_profile $HOME/.bash_profile
+	ln -s -f $HERE/bash_profile $HOME/.bashrc
+	ln -s -f $HERE/bash $HOME/.bash
+	ln -s -f $HERE/bash_completion /etc/bash_completion
+fi
+
+if [ "$VIM" != "n" ]; then
+	log "info" "Moving vim configs to tmp/backup\n"
+	mv -f $HOME/.vim $HOME/.vimrc $BKP_PATH
+	if [ -e "$(which vim)" ]; then
+		log "info" "Vim already installed\n"
+	else
+		if [ "$OSX" != "" ]; then
+			log "info" "Install vim...\n"
+			brew install macvim --env-std --with-override-system-vim --with-cscope --with-lua > /dev/null 2>&1
+		else
+			log "error" "Can't auto install vim\n"
+		fi
+	fi
+	log "info" "Seting new configs to vim\n"
+	ln -s -f $HERE/dotvim $HOME/.vim
+	ln -s -f $HERE/vimrc $HOME/.vimrc
 fi
 
 if [ "$TMUX" != "n" ]; then
 	if [ -e "$(which tmux)" ]; then
 		log "info" "TMUX already installed\n"
 	else
-		#if [ "$OSTYPE" =~ ^darwin ]; then
+		if [ "$OSX" != "" ]; then
 			log "info" "Install tmux...\n"
-			brew install tmux &>2
-		#else
-	#		log "error" "Can't auto install tmux\n"
-	#	fi
+			brew install tmux > /dev/null 2>&1
+		else
+			log "error" "Can't auto install tmux\n"
+		fi
 	fi
 	log "info" "Moving tmux configs to tmp/backup\n"
 	mv -f $HOME/.tmux.conf $BKP_PATH
-	log "info" "Seting new configs to ruby\n"
-	ln -sf $HERE/tmux.conf $HOME/.tmux.conf
+	log "info" "Seting new configs to tmux\n"
+	if [ "$TMUX_SIMPLE" != "n" ]; then
+		ln -s -f $HERE/tmux.simple.conf $HOME/.tmux.conf
+	else
+		ln -s -f $HERE/tmux.conf $HOME/.tmux.conf
+	fi
 fi
 
-log "info" "Moving others configs to tmp/backup\n"
-mv -f $HOME/.ssh/config $HOME/.vim $HOME/.vimrc $HOME/.eslintrc $BKP_PATH
-log "info" "Seting others new configs\n"
-ln -sf $HERE/sshconfig $HOME/.ssh/config
-ln -s $HERE/dotvim $HOME/.vim
-ln -sf $HERE/vimrc $HOME/.vimrc
-ln -sf $HERE/eslintrc $HOME/.eslintrc
+#log "info" "Moving others configs to tmp/backup\n"
+#mv -f $HOME/.ssh/config $HOME/.eslintrc $BKP_PATH
+#log "info" "Seting others new configs\n"
+#ln -s -f $HERE/sshconfig $HOME/.ssh/config
+#ln -s -f $HERE/eslintrc $HOME/.eslintrc
 
 log "success" "Defined all configs\n"
 
+if [ "$ZSH_VERSION" == "" ]; then
+	log "info" "Installing zsh\n"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 if [ "$ZSH" == "n" ]; then
 	source $HOME/.bash_profile
-#else
-	#source $HOME/.zshrc
 fi
+
+log "success" "Finished \o/"
